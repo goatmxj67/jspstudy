22edc5c0c9bb61572e22bb74706b1b99238cb956
goatmxj67 <yulki67@naver.com>
2021-05-11 16:03:49 +0900

    update 2021-05-11

--- workspace/01_SERVLET/src/quiz/Quiz4.java
+++ workspace/01_SERVLET/src/quiz/Quiz4.java
@@ -1,5 +1,8 @@
 package quiz;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
@@ -14,14 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Quiz03
  */
-@WebServlet("/Quiz03")
-public class Quiz03 extends HttpServlet {
+@WebServlet("/Quiz04")
+public class Quiz4 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Quiz03() {
+    public Quiz4() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -52,6 +55,26 @@ public class Quiz03 extends HttpServlet {
 				String date = new SimpleDateFormat("yyyy-MM-dd H:mm:ss").format(new Date()); // MM인 이유: mm -> minute
 				// H: 15시 hh: 03시
 				
+				// 생성할 파일명
+				String filename = date.substring(0, 10) + "_" + writer + ".txt";
+				
+				// 파일 경로
+				File file = new File("C:\\spring0303\\jspstudy", filename);
+				
+				// 파일 생성 스트림
+				BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+				
+				// 파일 생성
+				bw.write("작성일자: " + date + "\n"); // bw.newLine();
+				bw.write("작성IP: " + ip); bw.newLine();
+				bw.write("작성자: " + writer); bw.newLine();
+				bw.write("제목: " + title); bw.newLine();
+				bw.write(contents); bw.newLine();
+				
+				if (bw != null) {
+					bw.close();
+				}
+				
 				// 2. 응답 처리
 				
 				// 1) content-type
@@ -62,22 +85,10 @@ public class Quiz03 extends HttpServlet {
 				PrintWriter out = response.getWriter();
 				
 				// 3) 응답 생성
-				out.println("<!DOCTYPE html>");
-				out.println("<html>");
-				out.println("<head>");
-				out.println("<meta charset=\"utf-8\">");
-				out.println("<title>제목</title>");
-				out.println("</head>");
-				out.println("<body>");
-				out.println("<ul>");
-				out.println("<li>제목: " + title + "</li>");
-				out.println("<li>작성자: " + writer + "</li>");
-				out.println("<li>내용: " + contents + "</li>");
-				out.println("<li>IP주소: " + ip + "</li>");
-				out.println("<li>작성일자: " + date + "</li>");
-				out.println("</ul>");
-				out.println("</body>");
-				out.println("</html>");
+				out.println("<script>");
+				out.println("alert('" + filename + "파일이 생성되었습니다.')");
+				out.println("history.back()");
+				out.println("</script>");
 				out.flush();
 	}
 
