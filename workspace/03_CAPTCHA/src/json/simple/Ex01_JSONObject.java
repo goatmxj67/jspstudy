ea862e963e3a6995570fd1a1582800d83a762c93
goatmxj67 <yulki67@naver.com>
2021-05-17 14:53:49 +0900

    update 2021-05-17

---
+++ workspace/03_CAPTCHA/src/json/simple/Ex01_JSONObject.java
@@ -0,0 +1,29 @@
+package json.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex01_JSONObject {
+
+	public static void main(String[] args) {
+
+		// Map 리뷰
+		/*
+		 * Map<String, Object> map = new HashMap<>(); map.put("name", "브레드");
+		 * map.put("age", 50); System.out.println("이름: " + map.get("name"));
+		 * System.out.println("나이: " + map.get("age"));
+		 */
+		
+		// JSON 데이터
+		// 네이버가 우리에게 전달 해 준 JSON이라고 생각한다.
+		String responseData = "{\"name\": \"브레드\", \"age\": 50}";
+		// System.out.println(responseData);
+		// json-simple-1.1.1.jar 라이브러리
+		// 1. 구글에서 제공
+		// 2. JSON 데이터를 Java의 Map 형식으로 변환
+		// 3. Map 인터페이스를 구현한 JSONObject 클래스를 지원
+		
+		
+	}
+
+}
