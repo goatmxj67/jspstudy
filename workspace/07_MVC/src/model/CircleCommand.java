f70f0fc7e0dbd7ec8a3111a4d6a5565a2c71d07e
goatmxj67 <yulki67@naver.com>
2021-05-24 10:59:41 +0900

    update 2021-05-24

---
+++ workspace/07_MVC/src/model/CircleCommand.java
@@ -0,0 +1,27 @@
+package model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.ModelAndView;
+
+public class CircleCommand implements ShapeCommand {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		double radius = 0;
+		try {
+			radius = Double.parseDouble(request.getParameter("radius"));
+		} catch (Exception e) {
+			
+		}
+		
+		// 결과값을 request에 저장
+		request.setAttribute("area", Math.PI * Math.pow(radius, 2));
+		
+		return new ModelAndView("views/output.jsp", false);
+		
+	}
+
+}
