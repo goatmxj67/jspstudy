7a0c64e556b483ba211e0e561bf592ccf68bba0e
goatmxj67 <yulki67@naver.com>
2021-06-07 12:47:20 +0900

    update 2021-06-07

---
+++ workspace/13_AJAX/src/dao/MemberDAO.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import dto.Member;
+import mybatis.config.DBService;
+
+public class MemberDAO {
+
+	private SqlSessionFactory factory;
+	private static MemberDAO instance;
+	
+	private MemberDAO() {
+		factory = DBService.getInstance().getFactory();
+	}
+	
+	public static MemberDAO getInstance() {
+		if (instance == null) {
+			instance = new MemberDAO();
+		}
+		return instance;
+	}
+	
+	/* 1. 회원 목록 가져오기 */
+	public List<Member> selectMemberList() {
+		SqlSession ss = factory.openSession();
+		List<Member> list = ss.selectList("dao.member.selectMemberList");
+		ss.close();
+		return list;
+	}
+	
+	/* 2. 회원 정보 가져오기 */
+	public Member selectMemberByNo(long no) {
+		SqlSession ss = factory.openSession();
+		Member member = ss.selectOne("dao.member.selectMemberByNo", no);
+		ss.close();
+		return member;
+	}
+	
+	
+	
+	
+	
+}
